3a4ae8bc0e9da6c315f4359da70dc0e98116921f
20191645 <anfki2354@naver.com>
2024-06-24 15:47:01 +0900

    feat: 언리얼 프로젝트 생성

---
+++ Source/SecondGame/SecondGame.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SecondGame : ModuleRules
+{
+	public SecondGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/SecondGame/SecondGame.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SecondGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SecondGame, "SecondGame" );
+ 
